bbda249c16572964ac79676914453f937f7c62e4
Rupal Desai <35616791+RupalDesai@users.noreply.github.com>
2020-09-30 20:30:18 -0400

    Updates

---
+++ precache-manifest.214e0cc72cdc2ee524215bd147595c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2ad8734c7c51e954fc44bfc0662119",
+    "url": "/myreactapp/index.html"
+  },
+  {
+    "revision": "8fdb7ff776fe2b711c5a",
+    "url": "/myreactapp/static/css/main.02bd1249.chunk.css"
+  },
+  {
+    "revision": "f126f297ce880eb90da9",
+    "url": "/myreactapp/static/js/2.0ff2d895.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreactapp/static/js/2.0ff2d895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fdb7ff776fe2b711c5a",
+    "url": "/myreactapp/static/js/main.54244384.chunk.js"
+  },
+  {
+    "revision": "5abcf5dd1171be5f087d",
+    "url": "/myreactapp/static/js/runtime-main.944d8c5b.js"
+  }
+]);
